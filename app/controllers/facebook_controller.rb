4ffe7cfa01fb9fd84fabac30cc7759a05a7a299b
Expedito Andrade <expeditojazz@gmail.com>
2020-08-06 05:19:54 -0300

    add render to json

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,4 +1,12 @@
+require 'csv'
+
 class FacebookController < ApplicationController
   def data
+    newData = [];
+    # data = CSV.read('dados.csv')
+    CSV.foreach('dados.csv', headers: true, converters: :all) do |row|
+      newData << row.to_hash
+    end
+    render json: newData
   end
 end
